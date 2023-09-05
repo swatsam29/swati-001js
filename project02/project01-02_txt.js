5a5f2a2ebdb99e9cb2097b63e41b4445a2e92eeb
swatsam29 <ssampathkumar@uco.edu>
2023-09-05 03:17:26 -0500

    final

--- project02/project01-02_txt.js
+++ project02/project01-02_txt.js
@@ -2,10 +2,14 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Swathi SampathKumar
+    Date:   09/03/2023
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+let Basic = "50 Mbsp"
+let Express = "100 Mbsp";
+let Extreme = "500 Mbsp";
+let  Ultimate = "1 Gig";
